11a03e0b1333b2e74047e7dd464317ee63be350b
nanyovel <jperezjimenez06@gmail.com>
2024-06-14 13:44:57 -0400

    se modifico el registrador de existencia pero se volvio a colocar como estanba

--- src/App.jsx
+++ src/App.jsx
@@ -135,14 +135,13 @@ const App = () => {
       }
     }, [collectionName, setState, idUsuario]);
   };
-
+  useDocById('usuarios', setUserMaster,idUsuario);
   useDocByCondition('ordenesCompra', setDBOrdenes);
   useDocByCondition('usuarios', setDBUsuario);
   useDocByCondition('resennias', setDBResennias,'estadoDoc',"==",0);
   useDocByCondition('billOfLading', setDBBillOfLading, 'estadoDoc',"==",0);
   useDocByCondition('tutoriales', setDBTutoriales);
   let idUsuario=usuario?.uid?usuario.uid:'00';
-  useDocById('usuarios', setUserMaster,idUsuario);
 
   // // ******************** CONFIRMAR EMAIL ******************** //
   const confirmarEmail=()=>{
@@ -193,9 +192,8 @@ const App = () => {
           ...userMaster,
           registrosActividad:registrosActividad} );
     }
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[location]);
-
   return (
     <ContenedorPrincipal >
       <MenuLateral userMaster={userMaster}/>
