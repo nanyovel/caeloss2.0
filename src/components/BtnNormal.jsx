43928b41df19c700516cb3a2ba4ae30d50c1d9ee
nanyovel <jperezjimenez06@gmail.com>
2024-08-22 19:45:53 -0400

    normal luego de los cambios severos en estructura de carpeta

--- src/components/BtnNormal.jsx
+++ src/components/BtnNormal.jsx
@@ -1,50 +1,50 @@
-// import styled  from 'styled-components';
-// import { BtnGeneralButton } from './BtnGeneralButton';
+import styled  from 'styled-components';
+import { BtnGeneralButton } from './BtnGeneralButton';
 
-// export const BtnNormal = styled(BtnGeneralButton)`
-//     &.danger{
-//         background-color: red;
-//         color: white;
-//     &:hover{
-//         background-color: white;
-//         color:red;
-//     }
-//     &:active{
-//         background-color: #c52a2a;
-//         color: white;
-//     }
-//     }
-//     &.buscar{
-//         margin: 0;
-//     }
-// `;
+export const BtnNormal = styled(BtnGeneralButton)`
+    &.danger{
+        background-color: red;
+        color: white;
+    &:hover{
+        background-color: white;
+        color:red;
+    }
+    &:active{
+        background-color: #c52a2a;
+        color: white;
+    }
+    }
+    &.buscar{
+        margin: 0;
+    }
+`;
 
-// export const BtnNormalw = ({tipo,texto}) => {
-//   return (
-//     <BtnSingle
-//       className={tipo}
-//     >
-//       {texto}
-//     </BtnSingle>
-//   );
-// };
+export const BtnNormalw = ({tipo,texto}) => {
+  return (
+    <BtnSingle
+      className={tipo}
+    >
+      {texto}
+    </BtnSingle>
+  );
+};
 
-// const BtnSingle=styled(BtnGeneralButton)`
-// &.normal{
+const BtnSingle=styled(BtnGeneralButton)`
+&.normal{
 
-// }
-// &.danger{iiiiiii
-//     background-color: red;
-//     color: white;
-//     &:hover{
-//         background-color: white;
-//         color:red;
-//     }
-//     &:active{
-//         background-color: #c52a2a;
-//     color: white;
-//     }
-// }
-// &.buscar{
-//     margin: 0;
-//   }`;
+}
+&.danger{
+    background-color: red;
+    color: white;
+    &:hover{
+        background-color: white;
+        color:red;
+    }
+    &:active{
+        background-color: #c52a2a;
+    color: white;
+    }
+}
+&.buscar{
+    margin: 0;
+  }`;
